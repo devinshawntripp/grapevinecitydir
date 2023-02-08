470f37278cbebc887d2a9227447e2a94e02bef91
Devin <devinshawntripp@gmail.com>
2023-02-07 20:12:51 -0600

    changes

--- components/elements/BackToTop.js
+++ components/elements/BackToTop.js
@@ -1,34 +1,39 @@
 import { useEffect, useState } from "react";
 
 function BackToTop() {
-    const [hasScrolled, setHasScrolled] = useState("false");
-    useEffect(() => {
-        window.addEventListener("scroll", onScroll);
-        return () => {
-            window.removeEventListener("scroll", onScroll);
-        };
-    });
-
-    // const scrollToTop = () => {
-    //     window.scrollTo({ top: 0, behavior: "smooth" });
-    // };
-
-    const onScroll = () => {
-        if (window.scrollY > 100 && !hasScrolled) {
-            setHasScrolled(true);
-        } else if (window.scrollY < 100 && hasScrolled) {
-            setHasScrolled(false);
-        }
+  const [hasScrolled, setHasScrolled] = useState("false");
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
     };
+  });
 
-    return (
-        <>
-            {hasScrolled && (
-                <a id="scrollUp" href="#top" style={{ position: 'fixed', zIndex: 2147483647 }}>
-                    <i className="fi-rr-arrow-small-up" />
-                </a>
-            )}
-        </>
-    );
+  // const scrollToTop = () => {
+  //     window.scrollTo({ top: 0, behavior: "smooth" });
+  // };
+
+  const onScroll = () => {
+    if (window.scrollY > 100 && !hasScrolled) {
+      setHasScrolled(true);
+    } else if (window.scrollY < 100 && hasScrolled) {
+      setHasScrolled(false);
+      //testing
+    }
+  };
+
+  return (
+    <>
+      {hasScrolled && (
+        <a
+          id="scrollUp"
+          href="#top"
+          style={{ position: "fixed", zIndex: 2147483647 }}
+        >
+          <i className="fi-rr-arrow-small-up" />
+        </a>
+      )}
+    </>
+  );
 }
 export default BackToTop;
