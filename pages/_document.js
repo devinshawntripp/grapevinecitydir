2a9f3a7c403a33f961695d741283768c208cdea9
Devin Tripp <devinshawntripp@gmail.com>
2023-03-07 01:29:40 -0600

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,19 @@ export default function Document() {
           href="/assets/imgs/GvineImgs/Logos/svg/gcd-website-favicon-color.png"
         />
       </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2NYTEMEQVH"
+      ></Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-2NYTEMEQVH');
+      `}
+      </Script>
       <body>
         <Main />
         <NextScript />
